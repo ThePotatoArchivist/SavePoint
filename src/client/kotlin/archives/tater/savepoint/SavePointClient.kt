2bc0de191e5da46229b440661b3f26d271784262
JosiahFu <josiahfu@gmail.com>
2025-07-20 16:50:33 -0700

    Update icon and metadata

--- src/client/kotlin/archives/tater/savepoint/SavePointClient.kt
+++
@@ -1,9 +0,0 @@
-package archives.tater.savepoint
-
-import net.fabricmc.api.ClientModInitializer
-
-object SavePointClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
-}
